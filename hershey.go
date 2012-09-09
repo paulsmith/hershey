f9056eee17b69f37cca24063e140dc7408d2352a
Paul Smith <paulsmith@pobox.com>
2012-09-09 11:36:16 -0400

    gofmt

--- hershey.go
+++ hershey.go
@@ -1,45 +1,45 @@
 package hershey
 
 import (
-    "fmt"
-    "strings"
+	"fmt"
+	"strings"
 )
 
 type coord struct {
-    x, y int
+	x, y int
 }
 
 type Sym struct {
-    id, leftPos, rightPos int
-    coords []coord
+	id, leftPos, rightPos int
+	coords                []coord
 }
 
 func ReadSym(line string) (*Sym, error) {
-    var (
-        n, numVert int
-        x, y byte
-        err error
-    )
-    sym := new(Sym)
-    r := strings.NewReader(line)
-    n, err = fmt.Fscanf(r, " %4d %2d%c%c", &sym.id, &numVert, &x, &y)
-    if n != 4 {
-        return nil, fmt.Errorf("wanted to scan %d items, got %d", 4, n)
-    }
-    if err != nil {
-        return nil, fmt.Errorf("scanning symbol: %s", err)
-    }
-    sym.leftPos = int(x) - int('R')
-    sym.rightPos = int(y) - int('R')
-    for i := 0; i < numVert-1; i++ {
-        n, err = fmt.Fscanf(r, "%c%c", &x, &y)
-        if n != 2 {
-            return nil, fmt.Errorf("wanted to scan %d items, got %d", 2, n)
-        }
-        if err != nil {
-            return nil, fmt.Errorf("scanning coords in symbol: %s", err)
-        }
-        sym.coords = append(sym.coords, coord{int(x)-int('R'), int(y)-int('R')})
-    }
-    return sym, nil
+	var (
+		n, numVert int
+		x, y       byte
+		err        error
+	)
+	sym := new(Sym)
+	r := strings.NewReader(line)
+	n, err = fmt.Fscanf(r, " %4d %2d%c%c", &sym.id, &numVert, &x, &y)
+	if n != 4 {
+		return nil, fmt.Errorf("wanted to scan %d items, got %d", 4, n)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("scanning symbol: %s", err)
+	}
+	sym.leftPos = int(x) - int('R')
+	sym.rightPos = int(y) - int('R')
+	for i := 0; i < numVert-1; i++ {
+		n, err = fmt.Fscanf(r, "%c%c", &x, &y)
+		if n != 2 {
+			return nil, fmt.Errorf("wanted to scan %d items, got %d", 2, n)
+		}
+		if err != nil {
+			return nil, fmt.Errorf("scanning coords in symbol: %s", err)
+		}
+		sym.coords = append(sym.coords, coord{int(x) - int('R'), int(y) - int('R')})
+	}
+	return sym, nil
 }
--- hershey_test.go
+++ hershey_test.go
@@ -1,53 +1,53 @@
 package hershey
 
 import (
-    "testing"
-    "reflect"
+	"reflect"
+	"testing"
 )
 
 type ReadSymTest struct {
-    in string
-    sym Sym
+	in  string
+	sym Sym
 }
 
 var readSymTests = []ReadSymTest{
-    {
-        "    2 16MWOMOV ROMSMUNUPSQ ROQSQURUUSVOV",
-        Sym{2, -5, 5, []coord{
-            {-3,-5}, {-3,4}, {-50,0}, {-3,-5}, {1,-5}, {3,-4}, {3,-2},
-            {1,-1}, {-50,0}, {-3,-1}, {1,-1}, {3,0}, {3,3}, {1,4}, {-3,4},
-        }},
-    },
-    {
-        "    8  9MWOMOV RUMUV ROQUQ",
-        Sym{8, -5, 5, []coord{
-            {-3,-5}, {-3,4}, {-50,0}, {3,-5}, {3,4}, {-50,0}, {-3,-1}, {3,-1},
-        }},
-    },
-    {
-        "  223  3PTRLRW",
-        Sym{223, -2, 2, []coord{{0,-6}, {0,5},}},
-    },
-    {
-        " 2123 24F^JMN[ RKMNX RRMN[ RRMV[ RSMVX RZMV[ RGMNM RWM]M",
-        Sym{2123, -12, 12, []coord{
-            {-8,-5}, {-4,9}, {-50,0}, {-7,-5}, {-4,6}, {-50,0}, {0,-5},
-            {-4,9}, {-50,0}, {0,-5}, {4,9}, {-50,0}, {1,-5}, {4,6},
-            {-50,0}, {8,-5}, {4,9}, {-50,0}, {-11,-5}, {-4,-5}, {-50,0},
-            {5,-5}, {11,-5},
-        }},
-    },
+	{
+		"    2 16MWOMOV ROMSMUNUPSQ ROQSQURUUSVOV",
+		Sym{2, -5, 5, []coord{
+			{-3, -5}, {-3, 4}, {-50, 0}, {-3, -5}, {1, -5}, {3, -4}, {3, -2},
+			{1, -1}, {-50, 0}, {-3, -1}, {1, -1}, {3, 0}, {3, 3}, {1, 4}, {-3, 4},
+		}},
+	},
+	{
+		"    8  9MWOMOV RUMUV ROQUQ",
+		Sym{8, -5, 5, []coord{
+			{-3, -5}, {-3, 4}, {-50, 0}, {3, -5}, {3, 4}, {-50, 0}, {-3, -1}, {3, -1},
+		}},
+	},
+	{
+		"  223  3PTRLRW",
+		Sym{223, -2, 2, []coord{{0, -6}, {0, 5}}},
+	},
+	{
+		" 2123 24F^JMN[ RKMNX RRMN[ RRMV[ RSMVX RZMV[ RGMNM RWM]M",
+		Sym{2123, -12, 12, []coord{
+			{-8, -5}, {-4, 9}, {-50, 0}, {-7, -5}, {-4, 6}, {-50, 0}, {0, -5},
+			{-4, 9}, {-50, 0}, {0, -5}, {4, 9}, {-50, 0}, {1, -5}, {4, 6},
+			{-50, 0}, {8, -5}, {4, 9}, {-50, 0}, {-11, -5}, {-4, -5}, {-50, 0},
+			{5, -5}, {11, -5},
+		}},
+	},
 }
 
 func TestInput(t *testing.T) {
-    for _, test := range readSymTests {
-        sym, err := ReadSym(test.in)
-        if err != nil {
-            t.Error(err)
-        } else {
-            if !reflect.DeepEqual(test.sym, *sym) {
-                t.Errorf("scanning %s: expected %+v, got %+v", test.in, test.sym, sym)
-            }
-        }
-    }
+	for _, test := range readSymTests {
+		sym, err := ReadSym(test.in)
+		if err != nil {
+			t.Error(err)
+		} else {
+			if !reflect.DeepEqual(test.sym, *sym) {
+				t.Errorf("scanning %s: expected %+v, got %+v", test.in, test.sym, sym)
+			}
+		}
+	}
 }
